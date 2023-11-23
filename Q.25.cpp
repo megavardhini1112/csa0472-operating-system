bdbb3926da3ee413bf418241b23897b5a0abf0e8
megavardhini1112 <150839741+megavardhini1112@users.noreply.github.com>
2023-11-23 08:53:18 +0530

    Add files via upload

---
+++ Q.25.cpp
@@ -0,0 +1,24 @@
+#include <errno.h> 
+#include <fcntl.h> 
+#include <stdio.h> 
+#include <unistd.h> 
+
+extern int errno; 
+
+int main() 
+{ 
+	 
+	int fd = open("foo.txt", O_RDONLY | O_CREAT); 
+
+	printf("fd = %d\n", fd); 
+
+	if (fd == -1) { 
+		 
+		printf("Error Number % d\n", errno); 
+
+		
+		perror("Program"); 
+	} 
+	return 0; 
+}
+
